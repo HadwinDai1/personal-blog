b20708a231251472b5e58791a275b3fb6c9743f6
DaiHaoDong <2597378587@qq.com>
2024-05-07 13:41:32 +0800

    [feat]封装axios请求

--- personal-blog-front/src/utils/request.js
+++ personal-blog-front/src/utils/request.js
@@ -1,7 +1,31 @@
 import axios from "axios";
 
-const http = axios.create({
+const service = axios.create({
     baseURL: '/dev-api', // 设置基础 URL
     timeout: 60000,
+    headers: {
+        'Content-Type': 'application/json', // 设置默认请求头
+    },
     // 其他配置...
 });
+
+export const get = (url, params = {}) => {
+    return instance.get(url, { params })
+      .then(response => response.data)
+      .catch(error => {
+        console.error('GET request error:', error);
+        throw error;
+    });
+};
+
+// 封装 POST 请求
+export const post = (url, data = {}) => {
+    return instance.post(url, data)
+      .then(response => response.data)
+      .catch(error => {
+        console.error('POST request error:', error);
+        throw error;
+    });
+};
+
+export default service;
--- personal-blog-front/vue.config.js
+++ personal-blog-front/vue.config.js
@@ -2,7 +2,19 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 8081 // 你想要使用的新端口号
+    port: 80, // 你想要使用的新端口号
+    proxy: {
+      '/dev-api': {
+
+        // target: `http://10.0.85.95:8080`,
+         target: `http://localhost:8080`,
+         // target:`http://10.30.2.60:8080`,
+         changeOrigin: true,
+         pathRewrite: {
+           '^/dev-api' : '',
+         },
+       },
+    }
   },
   // configureWebpack: {
   //   name: "PersonalBlog",
